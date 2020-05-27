645459ccecf901875d84d6d6aaa96b0881448901
bachi6305 <bachi6305@gmail.com>
2020-05-27 07:58:45 -0700

    Added

--- src/com/java/TestPractice.java
+++ src/com/java/TestPractice.java
@@ -3,6 +3,6 @@ package com.java;
 public class TestPractice {
 
     public static void main(String[] args) {
-	    System.out.println("Hello Git Welcome...to ds_algos...");
+	    System.out.println("Hello Git Welcome...to ds_algos...Its branch/Master..");
     }
 }
