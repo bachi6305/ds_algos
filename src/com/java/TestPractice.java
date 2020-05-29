fa3db8d2aaf1d544aaa57b20affefa8a519e5140
Vijay <54402783+bachi6305@users.noreply.github.com>
2020-05-28 20:29:35 -0700

    Update TestPractice.java

--- src/com/java/TestPractice.java
+++ src/com/java/TestPractice.java
@@ -3,10 +3,6 @@ package com.java;
 public class TestPractice {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-	    System.out.println("Hello Git Welcome...to ds_algos...Its Branch/Master");
-=======
 	    System.out.println("Hello Git Welcome...to ds_algos...Its Branch/Master...From branch........From branch");
->>>>>>> ae44ca0... Update TestPractice.java
     }
 }
